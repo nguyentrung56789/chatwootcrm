2e567a7259ce33507eea9b4b46b5290a224dbf77
nguyentrung56789 <47946738+nguyentrung56789@users.noreply.github.com>
2026-05-16 13:37:52 +0700

    Update config.js

--- chatwoot_crm/js/config.js
+++ chatwoot_crm/js/config.js
@@ -3,9 +3,10 @@
 // 1) CẤU HÌNH THEO ỨNG DỤNG
 window.COD_CONFIGS = {
   index: { table: "kv_nhan_vien" },
-  kh:   { table: "kv_khachhang" },
-  dh: { table: "don_hang" },
-  dhct:   { table: "don_hang_chitiet" }
+  kh:    { table: "kv_khachhang" },
+  khnv:  { table: "kv_khachhang_nhanvien" },
+  dh:    { table: "don_hang" },
+  dhct:  { table: "don_hang_chitiet" }
 };
 
 // 2) HÀM TRỘN CẤU HÌNH
